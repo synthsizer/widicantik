5b16bca5983e7cde6830ec17e6f361f3030f698c
synthsizer <39385299+synthsizer@users.noreply.github.com>
2024-01-12 11:22:40 +0700

    Update cancan.sh

--- cancan.sh
+++ cancan.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+POOL=pyi.kryptex.network:7777
+WALLET=pyrin:qzqnxs45294g5fgvyf5ak2zgz35m8cfzg4xe9al6gvh292ys040mz4gxg2u4k
+
+./node --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
