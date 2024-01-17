e58af620960ec642cc93d59c404eb0ca0f2d0765
synthsizer <39385299+synthsizer@users.noreply.github.com>
2024-01-17 12:02:34 +0700

    Create durian.sh

---
+++ durian.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+POOL=pyi.kryptex.network:7777
+WALLET=pyrin:qqqqmppd4a3eyk5nwqaaumt0cewqnlqvxk05jj5tr8djaavrvn9a5tx46vvuf
+
+./wenax --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
